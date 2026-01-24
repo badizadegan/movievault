2ac0fa44d31291801c96ef661c3c5f82e60be66f
Fahimeh Badiezadegan <fahimeh.badizadegan@gmail.com>
2026-01-24 01:24:00 +0100

    chore(ui): set up MovieVault theme, colors, typography and dimens

---
+++ app/src/main/java/com/fahimeh/movievault/ui/design/Dimens.kt
@@ -0,0 +1,14 @@
+package com.fahimeh.movievault.ui.design
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val xs = 4.dp
+    val sm = 8.dp
+    val md = 12.dp
+    val lg = 16.dp
+    val xl = 24.dp
+
+    val cardRadius = 16.dp
+    val posterRadius = 18.dp
+}
--- app/src/main/java/com/fahimeh/movievault/ui/theme/Color.kt
+++ app/src/main/java/com/fahimeh/movievault/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.fahimeh.movievault.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Brand = Color(0xFF7C4DFF)          // Purple accent
+val Bg = Color(0xFF0B0B10)             // Background
+val Surface = Color(0xFF141421)        // Card surface
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val TextPrimary = Color(0xFFF2F2F7)
+val TextSecondary = Color(0xFFB8B8C7)
+
+val Outline = Color(0xFF2A2A3A)
