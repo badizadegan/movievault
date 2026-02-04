88f1760fc23770eb41a34412e530fbf0c78a3104
Fahimeh Badiezadegan <fahimeh.badizadegan@gmail.com>
2026-02-04 01:19:36 +0100

    fix(network): use single RetrofitClient as source of truth

--- app/src/main/java/com/fahimeh/movievault/di/NetworkModule.kt
+++ app/src/main/java/com/fahimeh/movievault/di/NetworkModule.kt
@@ -1,15 +1,8 @@
 package com.fahimeh.movievault.di
 
+import com.fahimeh.movievault.data.remote.RetrofitClient
 import com.fahimeh.movievault.data.remote.TmdbApi
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 
 object NetworkModule {
-
-    fun provideTmdbApi(): TmdbApi =
-        Retrofit.Builder()
-            .baseUrl("https://api.themoviedb.org/3/")
-            .addConverterFactory(MoshiConverterFactory.create())
-            .build()
-            .create(TmdbApi::class.java)
+    fun provideTmdbApi(): TmdbApi = RetrofitClient.api
 }
